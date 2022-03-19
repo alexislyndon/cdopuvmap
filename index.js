6370e77be1bd1acc531c984c3e1e575f7d1a4d23
alexislyndon <alexislyndon@gmail.com>
2022-03-19 12:10:02 +0800

    moved index.html to public

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app
     })
     
     .get('/', (req, res) => {
-        res.sendFile(path.join(__dirname+'/index.html'));
+        res.sendFile(path.join(__dirname+'/public/index.html'));
     });
     
     app.use(express.static(path.join(__dirname,'public')));
