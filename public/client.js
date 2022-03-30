eb5498f8be2beb0c95ceaa1bdc5ecd0e73c5e73f
Michael Pagaran <mmpagaran64@gmail.com>
2022-03-30 21:46:17 +0800

    added: itirenary buttons, refresh map when new itirenary

--- public/client.js
+++ public/client.js
@@ -66,7 +66,7 @@ const fetchroutes = fetch('/routes')
             }));
             let text = data[i].properties.route_name;
             let splitted = text.split('Via');
-            let elementID = cleanString(text);
+            let elementID = 'route_' + cleanString(text);
             // console.log(elementID);
             if (splitted.length == 2) { //check if 'route_name' have: 'Via westbound chuchu'
                 $('#routesOutputList').append('<li><span class="itemClickZone" id="' + elementID + '"><div class="outputItem"><img src="icons/jeepney.svg" alt="jeepney icon" class="jeepneyIcon "><p class="routeName" ><strong>' + splitted[0] + '<br></strong>Via' + splitted[1] + '</p></div></span></li>');
@@ -80,7 +80,7 @@ const fetchroutes = fetch('/routes')
 
         //need to add allRoutesLayers to map first before doing this loop
         for (let i = 0; i < data.length; i++) {
-            allRoutesArray[i].layer_id = cleanString(data[i].properties.route_name); //adds new attribute 'layer_id'
+            allRoutesArray[i].layer_id = 'route_' + cleanString(data[i].properties.route_name); //adds new attribute 'layer_id'
         }
     })
 
@@ -119,23 +119,36 @@ var stylistic = (leg_type, index) => {
         dashArray: "12 3 9"
     }
 }
-
+function clearItirenary(){
+    if (allItirenariesArray.length > 0) { 
+        removeAllItirenaryItem();
+        allItirenariesArray.forEach(itirenary => {
+            itirenary.remove();
+        });
+    }
+}
+var allItirenariesArray = [];
+var itirenaryNames = [];
 function getItineraries(o, d){
-    hideAllItem();
+    hideAllRouteItem();
     hideAllRouteLayers();
+    console.log('check length' + allItirenariesArray.length);
+    clearItirenary();
     var o = origin.getLatLng();
     var d = destination.getLatLng();
-    var allItirenariesArray = [];
+    
     fetch(`/itineraries?origin=${encodeURIComponent(`${o.lng} ${o.lat}`)}&destination=${encodeURIComponent(`${d.lng} ${d.lat}`)}`)
         .then(res => { return res.json() })
         .then(data => {
-            console.log(data.length);
-            console.log(data);
-            console.log(data[0].json.features.length);
-            console.log(data[0].json.features);
+            // console.log(data.length);
+            // console.log(data);
+            // console.log(data[0].json.features.length);
+            // console.log(data[0].json.features);
             for (let i = 0; i < data.length; ++i) { //loop for data[n]
+                let text = '';
+                allItirenariesArray[i] = L.layerGroup()  // 1 layer group = 2 walks, route's vertices/edges
                 for (let j = 0; j < data[i].json.features.length; ++j) { //loop for data[n].json.features[n]
-                    allItirenariesArray.push(L.geoJSON(data[i].json.features[j], {
+                    allItirenariesArray[i].addLayer(L.geoJSON(data[i].json.features[j], {
                         onEachFeature: function(feature, layer){
                             layer.on({
                                 'click': function(e){
@@ -146,16 +159,28 @@ function getItineraries(o, d){
                         },
                         style: stylistic(data[i].json.features[j].properties.leg_type, i)
                     }));
-                    let text = data[i].json.features[j].properties.route_name;
-                    console.log(text);
-                    // let splitted = text.split('Via');
-                    // let elementID = cleanString(text);
-                    // console.log(elementID);
+                    if (data[i].json.features[j].properties.route_name != null) { // get only the route_name
+                        text = data[i].json.features[j].properties.route_name;
+                    }
                 }
+                itirenaryNames[i] = text;
+                let splitted = text.split('Via');
+                let elementID = 'itirenary_' + cleanString(text);
+                console.log('added' + elementID);
+                if (splitted.length == 2) { //check if 'route_name' have: 'Via westbound chuchu'
+                    $('#journeyOutputList').append('<li><span class="itemClickZone" id="' + elementID + '"><div class="outputItem"><img src="icons/jeepney.svg" alt="jeepney icon" class="jeepneyIcon "><p class="routeName" ><strong>' + splitted[0] + '<br></strong>Via' + splitted[1] + '</p></div></span></li>');
+                } else {
+                    $('#journeyOutputList').append('<li><span class="itemClickZone" id="' + elementID + '"><div class="outputItem" ><img src="icons/jeepney.svg" alt="jeepney icon" class="jeepneyIcon "><p class="routeName" ><strong>' + splitted[0] + '<br></strong></p></div></span></li>');
+                }
+
             }
-            // allItirenariesArray.forEach(route => {
-            //     route.addTo(map);
-            // });
+            allItirenariesArray.forEach(itirenary => {
+                itirenary.addTo(map);
+            });
+            for (let i = 0; i < allItirenariesArray.length; i++) { // assign layer_id with gathered route names
+                allItirenariesArray[i].layer_id = 'itirenary_' + cleanString(itirenaryNames[i]);
+            }
+            console.log(allItirenariesArray);
         })
 }
 
@@ -180,9 +205,6 @@ var osmDefault = L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.
     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
 });
 
-var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-});
 // map initialization
 var map = L.map('map', {
     center: [8.477703150412395, 124.64379231398955], // target is rizal monument
@@ -191,13 +213,6 @@ var map = L.map('map', {
         osmDefault
     ]
 });
-
-var baseMaps = {
-    "Default": osmDefault,
-    "Satellite": Esri_WorldImagery
-}
-
-L.control.layers(baseMaps).addTo(map);
 
 var userMarker = L.marker([8.477703150412395, 124.64379231398955]);
 
@@ -283,7 +298,7 @@ function hideAllRouteLayers(){
     });
 }
 $('#hideAllBtn').click(function () {
-    hideAllItem();
+    hideAllRouteItem();
     hideAllRouteLayers();
 });
 function showAllRouteLayers(){
@@ -292,7 +307,7 @@ function showAllRouteLayers(){
     });
 }
 $('#showAllBtn').click(function () {
-    showAllItem();
+    showAllRouteItem();
     showAllRouteLayers();
 });
 
@@ -327,12 +342,29 @@ $(document).on('click', '.itemClickZone', function (e) {
         }
     }
 });
-function showAllItem(){
+function showAllItirenaryItem(){
+    allItirenariesArray.forEach(itirenary => {
+        $('#'+itirenary.layer_id).show();
+    });
+}
+function hideAllItirenaryItem(){
+    allItirenariesArray.forEach(itirenary => {
+        $('#'+itirenary.layer_id).hide();
+    });
+}
+function removeAllItirenaryItem(){
+    allItirenariesArray.forEach(itirenary => {
+        console.log('removed: ' + itirenary.layer_id);
+        $('#'+itirenary.layer_id).remove();
+
+    });
+}
+function showAllRouteItem(){
     allRoutesArray.forEach(route => {
         $('#'+route.layer_id).show();
     });
 }
-function hideAllItem(){
+function hideAllRouteItem(){
     allRoutesArray.forEach(route => {
         $('#'+route.layer_id).hide();
     });
