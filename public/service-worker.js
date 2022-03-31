a55909bb360aba57ada7de895e7f8d27ebedbfd8
alexislyndon <alexislyndon@gmail.com>
2022-03-31 20:51:39 +0800

    workbox png cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,25 @@ importScripts(
     'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
 
+// workbox.routing.registerRoute(
+//     ({ request }) => request.destination === 'image',
+//     new workbox.strategies.CacheFirst()
+// );
+
 workbox.routing.registerRoute(
-    ({ request }) => request.destination === 'image',
-    new workbox.strategies.CacheFirst()
+    // Cache image files.
+    /\.(?:png|jpg|jpeg|svg|gif)$/,
+    // Use the cache if it's available.
+    new workbox.strategies.CacheFirst({
+        // Use a custom cache name.
+        cacheName: 'image-cache',
+        plugins: [
+            new workbox.expiration.Plugin({
+                // Cache only 20 images.
+                maxEntries: 1500,
+                // Cache for a maximum of a week.
+                maxAgeSeconds: 7 * 24 * 60 * 60,
+            })
+        ],
+    })
 );
