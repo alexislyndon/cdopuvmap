62ca3c8dc88ced25797e8a1760a33ebd9ca48fde
alexislyndon <alexislyndon@gmail.com>
2022-04-01 07:01:51 +0800

    yet another service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,20 +7,32 @@ importScripts(
 //     new workbox.strategies.CacheFirst()
 // );
 
+// workbox.routing.registerRoute(
+//     // Cache image files.
+//     /\.(?:png\?key=.+)$/,
+//     // Use the cache if it's available.
+//     new workbox.strategies.CacheFirst({
+//         // Use a custom cache name.
+//         cacheName: 'image-cache',
+//         plugins: [
+//             new workbox.expiration.ExpirationPlugin({
+//                 // Cache only 20 images.
+//                 maxEntries: 1500,
+//                 // Cache for a maximum of a week.
+//                 maxAgeSeconds: 7 * 24 * 60 * 60,
+//             })
+//         ],
+//     })
+// );
+
 workbox.routing.registerRoute(
-    // Cache image files.
-    /\.(?:png\?key=.+)$/,
-    // Use the cache if it's available.
-    new workbox.strategies.CacheFirst({
-        // Use a custom cache name.
-        cacheName: 'image-cache',
+    /^https:\/\/api.maptiler.com\/.*/,
+    workbox.strategies.cacheFirst({
+        cacheName: 'tile-cache',
         plugins: [
-            new workbox.expiration.ExpirationPlugin({
-                // Cache only 20 images.
-                maxEntries: 1500,
-                // Cache for a maximum of a week.
-                maxAgeSeconds: 7 * 24 * 60 * 60,
+            new workbox.cacheableResponse.Plugin({
+                statuses: [0, 200]
             })
-        ],
-    })
+        ]
+    }),
 );
