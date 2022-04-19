2ba7754011ffd90f1fe8e734fef2bec18084296b
beebang <delizoarbie@gmail.com>
2022-04-19 21:08:53 +0800

    modal for user report

--- public/client.js
+++ public/client.js
@@ -552,3 +552,23 @@ var dDrag = function (e) {
     var center = map.getCenter()
     destination.setLatLng(center)
 }
+
+//script for the modal user report
+
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector(".trigger");
+const closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
