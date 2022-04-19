bc3d6305c6195cd88d696ed7b06fa95ed3485027
alexislyndon <alexislyndon@gmail.com>
2022-04-19 20:59:43 +0800

    edit/save works on the same tr only!!

--- public/admin.js
+++ public/admin.js
@@ -1,22 +1,22 @@
 $.ajaxSetup({
     beforeSend: function () {
-      $("#spinner").show();
+        $("#spinner").show();
     },
     complete: function () {
-      $("#spinner").fadeOut();
+        $("#spinner").fadeOut();
     },
-  });
+});
 
-$(function() {
+$(function () {
 
     $.ajaxSetup({
         beforeSend: function () {
-          $("#spinner").show();
+            $("#spinner").removeAttr('hidden')
         },
-        complete: function () {
-          $("#spinner").fadeOut();
+        always: function () {
+            $("#spinner").attr('hidden')
         },
-      });
+    });
 
     $('#routes').on('click', (params) => {
         $('#main').load('/admin/routes')
@@ -24,4 +24,42 @@ $(function() {
     $('#reports').on('click', (params) => {
         $('#main').load('/admin/reports')
     })
+});
+
+$(document).on('click', '.edit', function () {
+    $(this).parent().siblings('td.data').each(function () {
+        if ($(this).hasClass('data')) {
+            // do stuff here to output to the page
+            var content = $(this).html().trim();
+            $(this).html('<input value="' + content + '" />');
+        }
+    });
+
+    $(this).siblings('.save').show();
+    $(this).siblings('.delete').hide();
+    $(this).hide();
+});
+
+$(document).on('click', '.save', function () {
+    var row = $(this).closest('tr');
+    $(this).closest('tr').find('input').each(function () {
+        // if($(this).parent().parent() != row) return
+        var content = $(this).val();
+        if(!content) {$(this).remove(); return}
+        $(this).html(content);
+        $(this).contents().unwrap();
+    });
+    $(this).siblings('.edit').show();
+    $(this).siblings('.delete').show();
+    $(this).hide();
+
+});
+
+
+$(document).on('click', '.delete', function () {
+    $(this).parents('tr').remove();
+});
+
+$('.add').click(function () {
+    $(this).parents('table').append('<tr><td class="data"></td><td class="data"></td><td class="data"></td><td><button class="save">Save</button><button class="edit">Edit</button> <button class="delete">Delete</button></td></tr>');
 });
