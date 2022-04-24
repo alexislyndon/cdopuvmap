78ab1a0f9ba290de553a2a575855bc22f7a79fe1
alexislyndon <alexislyndon@gmail.com>
2022-04-25 06:20:33 +0800

    final-alexis wrong password <p>

--- public/login.js
+++ public/login.js
@@ -1,15 +1,18 @@
-$(function() {
+$(function () {
 
-    $('form').on('submit', function(e) {
+    $('form').on('submit', function (e) {
         e.preventDefault()
         $.ajax({
             url: '/login',
             method: 'POST',
             data: $(this).serialize(),
-            success: function(data) {
-                if(data.user){
+            success: function (data) {
+                if (data.user) {
                     location.assign('/admin')
                 }
+            },
+            error: function () {
+                $('#m').fadeIn().delay(1800).fadeOut();
             }
 
         })
