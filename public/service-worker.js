0491b46d34ec77c4203beb4ba949d21114820b20
alexislyndon <alexislyndon@gmail.com>
2022-04-25 15:22:44 +0800

    serviceworker edit #3

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,19 +3,6 @@ importScripts(
 );
 
 workbox.routing.registerRoute(
-    /^https:\/\/api.maptiler.com\/maps.*/,
-    new workbox.strategies.CacheFirst({
-        cacheName: 'tile-cache',
-        plugins: [
-            new workbox.cacheableResponse.CacheableResponsePlugin({
-                statuses: [0, 200]
-            })
-        ]
-    }),
-);
-
-
-workbox.routing.registerRoute(
     /\/routes/,
     new workbox.strategies.CacheFirst({
         cacheName: 'routes-cache',
@@ -28,35 +15,34 @@ workbox.routing.registerRoute(
 );
 
 workbox.routing.registerRoute(
+    /^https:\/\/api.maptiler.com\/maps.*/,
+    new workbox.strategies.CacheFirst({
+        cacheName: 'tile-cache',
+        plugins: [
+            new workbox.cacheableResponse.CacheableResponsePlugin({
+                statuses: [0, 200]
+            })
+        ]
+    }),
+);
+
+workbox.routing.registerRoute(
     /\.html/,
     new workbox.strategies.CacheFirst({
-      cacheName: 'html-cache',
+        cacheName: 'html-cache',
     })
-  );
-  
-  workbox.routing.registerRoute(
+);
+
+workbox.routing.registerRoute(
     /\.css$/,
     new workbox.strategies.CacheFirst({
-      cacheName: 'css-cache',
+        cacheName: 'css-cache',
     })
-  );
-  
-  workbox.routing.registerRoute(
+);
+
+workbox.routing.registerRoute(
     /\.js/,
     new workbox.strategies.CacheFirst({
-      cacheName: 'js-cache',
+        cacheName: 'js-cache',
     })
-  );
-
-  workbox.routing.registerRoute(
-    /\.(?:png|jpg|jpeg|svg|gif)$/,
-    new workbox.strategies.CacheFirst({
-      cacheName: 'image-cache',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxEntries: 20,
-          maxAgeSeconds: 7 * 24 * 60 * 60,
-        })
-      ],
-    })
-  );
+);
