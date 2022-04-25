4820cd63e8cfe66064afeff213bd7c958074ab36
alexislyndon <alexislyndon@gmail.com>
2022-04-25 15:14:41 +0800

    edit service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,3 +13,50 @@ workbox.routing.registerRoute(
         ]
     }),
 );
+
+
+workbox.routing.registerRoute(
+    ({url}) => url.pathname.startsWith('/routes'),
+    new workbox.strategies.CacheFirst({
+        cacheName: 'routes-cache',
+        plugins: [
+            new workbox.cacheableResponse.CacheableResponsePlugin({
+                statuses: [0, 200]
+            })
+        ]
+    }),
+);
+
+workbox.routing.registerRoute(
+    /\.html/,
+    new workbox.strategies.CacheFirst({
+      cacheName: 'html-cache',
+    })
+  );
+  
+  workbox.routing.registerRoute(
+    /\.css$/,
+    new workbox.strategies.CacheFirst({
+      cacheName: 'css-cache',
+    })
+  );
+  
+  workbox.routing.registerRoute(
+    /\.js/,
+    new workbox.strategies.CacheFirst({
+      cacheName: 'js-cache',
+    })
+  );
+
+  workbox.routing.registerRoute(
+    /\.(?:png|jpg|jpeg|svg|gif)$/,
+    new workbox.strategies.CacheFirst({
+      cacheName: 'image-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 20,
+          maxAgeSeconds: 7 * 24 * 60 * 60,
+        })
+      ],
+    })
+  );
