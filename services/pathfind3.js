d1af07ae924791809ba67b1d6cee754e4d9bcdc3
alexislyndon <alexislyndon@gmail.com>
2022-05-18 23:16:06 +0800

    0 3 slice

--- services/pathfind3.js
+++ services/pathfind3.js
@@ -152,7 +152,7 @@ module.exports = async (olon, olat, dlon, dlat, maxwalk = 300) => {
             console.log(`route:${r.json.features[0].properties.route_code} cost: ${r.json.features[le-1].properties.agg_cost}`);
         })
         // console.log(itinerary);
-        return itinerary
+        return itinerary.slice(0,3)
     }
 
 }
